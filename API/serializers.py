002ec11108667d906ae61b18f2da9b363d0682fd
Dara Thomas <darathomas@Daras-MacBook-Air.local>
2017-04-24 15:09:02 -0500

    Product_Type Class added and functional

--- API/serializers.py
+++ API/serializers.py
@@ -18,3 +18,9 @@ class CustomerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Customer
         fields = ('url', 'First_Name', 'Last_Name', 'Created')  
+
+class Product_TypeSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Product_Type
+        exclude = ()  
